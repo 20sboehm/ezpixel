25cc0bd765f0ada37738b1ce239a8324d15ce6bc
20sboehm <46303896+20sboehm@users.noreply.github.com>
2022-12-30 18:07:37 -0700

    Bug fixes, Color Picker, Set sprite size

--- public/canvas.js
+++ public/canvas.js
@@ -1,31 +1,33 @@
 "use strict";
-let canvas = document.getElementById("canvas");
-let penBtn = document.getElementById("penBtn");
-let eraserBtn = document.getElementById("eraserBtn");
+let canvas = document.getElementById('canvas');
+let canvasBG = document.getElementById('canvasBG');
+let penBtn = document.getElementById('penBtn');
+let eraserBtn = document.getElementById('eraserBtn');
+let colorSelect = document.getElementById('colorSelect');
+let setColorBtn = document.getElementById('setColor');
+let selectSpriteSizeBtn = document.getElementById('spriteSizeBtn');
+let spriteSizeSelect = document.getElementById('spriteSizeSelect');
 let rect = canvas.getBoundingClientRect();
-let painter = canvas.getContext("2d");
+let painter = canvas.getContext('2d');
+let painterBG = canvasBG.getContext('2d');
 let mouseX = 0;
 let mouseY = 0;
 let inputCheckInterval = setInterval(handleDrawInput, 5);
 let pixelData = [[]];
 let currentTool;
 let mouseDown = false;
-let penColor = "rgba(0, 0, 0, 1)";
+let penColor = 'rgba(0, 0, 0, 1)';
+let spriteSize = 16;
+let scaleFactor = canvas.width / spriteSize;
 var ToolType;
 (function (ToolType) {
     ToolType[ToolType["Pen"] = 0] = "Pen";
     ToolType[ToolType["Eraser"] = 1] = "Eraser";
 })(ToolType || (ToolType = {}));
-painter.scale(32, 32);
 currentTool = ToolType.Pen;
-// Initialize the canvas with opacity-0 color values
-for (let i = 0; i < 16; i++) {
-    pixelData[i] = [];
-    for (let j = 0; j < 16; j++) {
-        pixelData[i][j] = "rgba(0, 0, 0, 0)";
-    }
-}
-window.addEventListener("mousemove", function (event) {
+// Set up everything we need for the canvas and its initial display
+setupCanvas();
+window.addEventListener('mousemove', function (event) {
     mouseX = event.x - rect.x;
     mouseY = event.y - rect.y;
 });
@@ -35,18 +37,70 @@ document.body.onmousedown = function () {
 document.body.onmouseup = function () {
     mouseDown = false;
 };
-penBtn.addEventListener("click", function () {
+// Keep track of where the canvas is releative to the browser window to make sure the coordinate math stays accurate
+window.onresize = function (event) {
+    rect = canvas.getBoundingClientRect();
+};
+window.addEventListener('blur', function (event) {
+    mouseDown = false;
+});
+penBtn.addEventListener('click', function () {
     currentTool = ToolType.Pen;
 });
-eraserBtn.addEventListener("click", function () {
+eraserBtn.addEventListener('click', function () {
     currentTool = ToolType.Eraser;
-    console.log("eraser selected");
 });
+selectSpriteSizeBtn.addEventListener('click', function () {
+    painter.scale(1 / scaleFactor, 1 / scaleFactor); // "Unscale" the canvas before rescaling it to its new scaling in the setupCanvas() method
+    painterBG.scale(1 / scaleFactor, 1 / scaleFactor);
+    spriteSize = parseInt(spriteSizeSelect.value);
+    scaleFactor = canvas.width / spriteSize;
+    painter.clearRect(0, 0, spriteSize, spriteSize);
+    painterBG.clearRect(0, 0, spriteSize, spriteSize);
+    pixelData = [[]];
+    setupCanvas();
+});
+setColorBtn.addEventListener('click', function () {
+    penColor = colorSelect.value;
+});
+//
+// ---------- FUNCTIONS ----------
+//
+function setupCanvas() {
+    painter.scale(scaleFactor, scaleFactor);
+    painterBG.scale(scaleFactor, scaleFactor);
+    // Initialize the canvas with opacity-0 color values
+    for (let i = 0; i < spriteSize; i++) {
+        pixelData[i] = [];
+        for (let j = 0; j < spriteSize; j++) {
+            pixelData[i][j] = 'rgba(0, 0, 0, 0)';
+        }
+    }
+    drawBG();
+    drawSprite();
+}
+// Draw Background
+function drawBG() {
+    painterBG.fillStyle = "rgb(255, 255, 255, 1)";
+    painterBG.fillRect(0, 0, spriteSize, spriteSize);
+    for (let i = 0; i < spriteSize; i++) {
+        for (let j = 0; j < spriteSize; j++) {
+            if (((i + j) % 2) == 0) {
+                painterBG.fillStyle = "rgb(30, 30, 30, .1)";
+                painterBG.fillRect(i, j, 1, 1);
+            }
+            else {
+                painterBG.fillStyle = "rgb(30, 30, 30, .2)";
+                painterBG.fillRect(i, j, 1, 1);
+            }
+        }
+    }
+}
 // Draw data stored in pixelData
 function drawSprite() {
-    painter.clearRect(0, 0, 16, 16);
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+    painter.clearRect(0, 0, spriteSize, spriteSize);
+    for (let i = 0; i < spriteSize; i++) {
+        for (let j = 0; j < spriteSize; j++) {
             painter.fillStyle = pixelData[i][j];
             painter.fillRect(i, j, 1, 1);
         }
@@ -55,15 +109,15 @@ function drawSprite() {
 // Input pixel data
 function handleDrawInput() {
     if (mouseDown && mouseInBounds()) {
-        console.log("X: " + mouseX + " | Y: " + mouseY);
-        let pixelX = Math.floor((mouseX) / 32);
-        let pixelY = Math.floor((mouseY) / 32);
+        //console.log('X: ' + mouseX + ' | Y: ' + mouseY);
+        let pixelX = Math.floor((mouseX) / scaleFactor);
+        let pixelY = Math.floor((mouseY) / scaleFactor);
         if (mouseInBounds()) {
             if (currentTool == ToolType.Pen) {
                 pixelData[pixelX][pixelY] = penColor;
             }
             else if (currentTool == ToolType.Eraser) {
-                pixelData[pixelX][pixelY] = "rgba(0, 0, 0, 0)";
+                pixelData[pixelX][pixelY] = 'rgba(0, 0, 0, 0)';
             }
         }
         drawSprite();
